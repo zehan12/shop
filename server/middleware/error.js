f667c49cba9735ce540c81890028771580f94e6e
Zehan Khan <zkhan@hootboard.com>
2024-01-29 21:26:02 +0530

    feat: :sparkles: handle errors

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,10 +1,24 @@
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
+  console.log(err.name);
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
+
+  if (err.name === "CastError") {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    err = new ErrorHandler(message, err.statusCode);
+  }
+
+  if (err.name === "ValidationError") {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = `Invalid input data. ${errors.join(". ")}`;
+    err = new ErrorHandler(message, err.statusCode);
+  }
+
   res.status(err.statusCode).json({
-    error: true,
+    type: "Internal error",
+    status: "error",
     message: err.message,
   });
 };
